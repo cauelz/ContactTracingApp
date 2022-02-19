3175719f6e8aeaeafa8e001805f81ac825f20bdd
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2022-02-19 18:43:02 -0300

    yaml config e build folder

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,6 +1,6 @@
 
 echo "Install SFDX Scanner"
-echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
+sfdx plugins:install @salesforce/sfdx-scanner
 
 echo "Running SFDX Scanner"
 npx sfdx scanner:run --target "**/default/**" --format "csv" --outfile "sfdxScannerAnalysis.csv" --violations-cause-error
