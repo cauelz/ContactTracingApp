61bd3464aae3762988e159f0c65300796d6f19e0
Cauê Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2022-04-02 11:29:29 -0300

    bound and unbound expressions

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,5 +1,17 @@
 ({
-    myAction : function(component, event, helper) {
+    startGame : function(component, event, helper) {
 
+        let gameModeCombobox =  component.find("gameMode");
+        let selectedValue = gameModeCombobox.get("v.value");
+
+        component.set("v.selectedMode", selectedValue);
+
+        console.log("The start new game button was clicked, The Game Mode is: ", selectedValue);
+        
+        console.log(component.get("v.selectedMode"));
+    },
+
+    reshuffleBoard: function(component, event, helper) {
+        console.log("Reshuffle board is called");
     }
 })
