1e7b01858d734674dbbc7c56b51b5218b4324489
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2022-11-07 15:25:34 -0300

    added Board component

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    doInit : function(component, event, helper) {
+        console.log('Initialization completed');
+        helper.getWords(6);
+    },
+    doRender : function(component, event, helper) {
+        console.log('Render completed');
+    },
 })
