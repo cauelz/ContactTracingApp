794a0ff1040881f4861d1acce287f4c039bd8433
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2022-11-11 09:12:28 -0300

    created a few logics for the board component and added styles

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,7 +1,11 @@
 ({
     doInit : function(component, event, helper) {
         console.log('Initialization completed');
-        helper.getWords(6);
+        const words = helper.getWords(6); 
+        component.set("v.words", words);
+        console.log('Words: ' + words);
+        console.log('Win Word: ' + helper.getWinWord(words));
+        component.set("v.winWord", helper.getWinWord(words));
     },
     doRender : function(component, event, helper) {
         console.log('Render completed');
