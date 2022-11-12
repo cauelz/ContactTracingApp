0dd3640c3ac15447a48c38633660561dce3d9e3b
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2022-11-12 15:13:51 -0300

    learning about aura:iteration

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,10 +1,28 @@
 ({
     doInit : function(component, event, helper) {
         console.log('Initialization completed');
-        const words = helper.getWords(6); 
+
+        let column = 0;
+
+        const gameMode = component.get("v.mode");
+        
+        if(gameMode && gameMode === "hard") {
+            column = 6;
+        } else if(gameMode === "medium") {
+            column = 4;
+        } else {
+            column = 3;
+        }
+
+        let blockSize = 12/column;
+        component.set("v.blockSize", blockSize);
+
+        const words = helper.getWords(column*column); 
         component.set("v.words", words);
+
         console.log('Words: ' + words);
         console.log('Win Word: ' + helper.getWinWord(words));
+
         component.set("v.winWord", helper.getWinWord(words));
     },
     doRender : function(component, event, helper) {
